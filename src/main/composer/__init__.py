672e795eb2b43eee4483996e27a61ecccef1ff3f
Julio Felipe <julio@informach.com.br>
2021-10-27 19:57:40 -0300

    feat: Implementing first Composer and setting Flask

---
+++ src/main/composer/__init__.py
@@ -0,0 +1,5 @@
+from .register_user_composite import register_user_composer
+
+# from .register_pet_composite import register_pet_composer
+# from .find_pet_composite import find_pet_composer
+# from .find_user_composite import find_user_composer
